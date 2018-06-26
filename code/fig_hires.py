494d7fba6bba32e41561afe502cbc5f2a6215d91
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-06-26 14:19:38 +0200

    Updated files and added free-Na retrieval.

--- code/fig_hires.py
+++ code/fig_hires.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as si
 from scipy.ndimage.filters import gaussian_filter1d as gaussf
-#plt.ioff()
+plt.ioff()
 
 sys.path.append("../pyratbay/modules/MCcubed")
 import MCcubed.utils as mu
@@ -19,40 +19,61 @@ import pyratbay.constants as pc
 # High resolution RT model:
 pyrat = pb.pbay.run("spectrum_bestfit.cfg")
 
-# Double normalization of high-resolution data:
-pyrat.spec.hires = (1-pyrat.spec.spectrum) / (1-pyrat.spec.spectrum[0])
-pyrat.spec.hires = gaussf(pyrat.spec.hires, pyrat.phy.sigma)
 vel = pyrat.phy.rvsystem
 wn  = pyrat.spec.wn / ((1-vel/pc.c) / np.sqrt(1-(vel/pc.c)**2))
-finterp = si.interp1d(wn, pyrat.spec.hires)
-pyrat.obs.bandflux = finterp(pyrat.obs.bandwn)
+bandwl = 1/(pyrat.obs.bandwn[1:]*pc.A)
+uncert = pyrat.obs.uncert[1:]
+data   = pyrat.obs.data[1:]
 
 # N-sigma boundaries around Na lines:
+N = 3.0
 wn0 = np.array(pyrat.alkali.model[0].wn)
 wn1 = wn0 / ((1-vel/pc.c) / np.sqrt(1-(vel/pc.c)**2))
 wl1 = 1/(wn1*pc.A)
 dwl = 1/(wn0*pc.A) / pyrat.phy.resolution
-N = 3.0
-
 wlran1 = wl1[0] - N*dwl[0], wl1[0] + N*dwl[0]
 wlran2 = wl1[1] - N*dwl[1], wl1[1] + N*dwl[1]
 
-bandwl = 1/(pyrat.obs.bandwn[1:]*pc.A)
-flux   = pyrat.obs.bandflux[1:]
-uncert = pyrat.obs.uncert[1:]
-data   = pyrat.obs.data[1:]
+
+# Best-fit value spectrum:
+pyrat.atm.temp[:] = 1550.0
+pyrat.phy.rplanet = 129200 * pc.km
+pyrat = pb.pyrat.run(pyrat, [pyrat.atm.temp, pyrat.atm.q])
+# Double normalization of high-resolution data:
+pyrat.spec.hires = gaussf((1-pyrat.spec.spectrum) / (1-pyrat.spec.spectrum[0]),
+                          pyrat.phy.sigma)
+pyrat.obs.bandflux = si.interp1d(wn,pyrat.spec.hires)(pyrat.obs.bandwn)
+flux = np.copy(pyrat.obs.bandflux[1:])
+
+# Spectra for R0 5% larger/smaller:
+pyrat.phy.rplanet = 1.05 * 129200 * pc.km
+pyrat = pb.pyrat.run(pyrat, [pyrat.atm.temp, pyrat.atm.q])
+pyrat.spec.hires = gaussf((1-pyrat.spec.spectrum) / (1-pyrat.spec.spectrum[0]),
+                          pyrat.phy.sigma)
+pyrat.obs.bandflux = si.interp1d(wn,pyrat.spec.hires)(pyrat.obs.bandwn)
+hiflux = np.copy(pyrat.obs.bandflux[1:])
+
+pyrat.phy.rplanet = 0.95 * 129200 * pc.km
+pyrat = pb.pyrat.run(pyrat, [pyrat.atm.temp, pyrat.atm.q])
+pyrat.spec.hires = gaussf((1-pyrat.spec.spectrum) / (1-pyrat.spec.spectrum[0]),
+                          pyrat.phy.sigma)
+pyrat.obs.bandflux = si.interp1d(wn,pyrat.spec.hires)(pyrat.obs.bandwn)
+loflux = np.copy(pyrat.obs.bandflux[1:])
+
 
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # The figure:
 fs = 12
-
-plt.figure(3, (8.5, 3.5))
+lw = 1.5
+plt.figure(3, (8.5, 3.1))
 plt.clf()
 plt.subplots_adjust(0.1, 0.15, 0.95, 0.97, wspace=0.05)
 ax=plt.subplot(121)
-plt.plot(bandwl, flux, color="b", lw=1.5)
+plt.plot(bandwl, flux,   color="b",         lw=lw)
+plt.plot(bandwl, hiflux, color="orangered",    lw=lw, zorder=-5)
+plt.plot(bandwl, loflux, color="forestgreen", lw=lw, zorder=-5)
 plt.errorbar(bandwl, data, uncert, fmt="ko", ms=3, ecolor="0.3")
-plt.axvspan(wlran2[0], wlran2[1], color="0.7")
+plt.axvspan(wlran2[0], wlran2[1], color="0.85", zorder=-6)
 xticks = [5887, 5888, 5889, 5890, 5891]
 ax.set_xticks(xticks)
 ax.set_xticklabels(np.asarray(xticks, str))
@@ -61,10 +82,13 @@ plt.ylim(0.97, 1.02)
 plt.ylabel(r"$R$", fontsize=fs)
 plt.ylabel(r"$\tilde{\mathfrak{R}}$", fontsize=fs)
 plt.xlabel(r"$\rm Wavelength\ \ (A)$", fontsize=fs)
+# Right panel:
 ax=plt.subplot(122)
-plt.plot(bandwl, flux, color="b", lw=1.5)
+plt.plot(bandwl, flux,   color="b",         lw=lw)
+plt.plot(bandwl, hiflux, color="orangered", lw=lw, zorder=-5)
+plt.plot(bandwl, loflux, color="forestgreen", lw=lw, zorder=-5)
 plt.errorbar(bandwl, data, uncert, fmt="ko", ms=3, ecolor="0.3")
-plt.axvspan(wlran1[0], wlran1[1], color="0.7")
+plt.axvspan(wlran1[0], wlran1[1], color="0.85", zorder=-6)
 ax.set_yticklabels([])
 xticks = [5893, 5894, 5895, 5896, 5897]
 ax.set_xticks(xticks)
@@ -72,5 +96,4 @@ ax.set_xticklabels(np.asarray(xticks, str))
 plt.xlim(5893.25, 5896.75)
 plt.ylim(0.97, 1.02)
 plt.xlabel(r"$\rm Wavelength\ \ (A)$", fontsize=fs)
-plt.savefig("../plots/bestfit_spectrum.ps")
-
+plt.savefig("../plots/WASP-17b_hires_fix-Na_spectrum.pdf")
--- code/fig_post.py
+++ code/fig_post.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 plt.ioff()
 
 sys.path.append("../pyratbay/modules/MCcubed")
-import MCcubed.plots as mp
 import MCcubed.utils as mu
 sys.path.append("../pyratbay/modules/MCcubed/MCcubed/plots")
 import colormaps as cm
@@ -20,64 +19,155 @@ palette.set_under(color='w')
 palette.set_bad(color='w')
 
 rjup = 7.1492e4
-burnin = 5000
-#post = np.load("../retrieval_v03_3HW/MCMC_WASP17b_hires.npz")
-post = np.load("../run/MCMC_WASP17b_hires.npz")
+burnin  = 5000
+nbins   = 35
+nlevels = 20
+
+# Fixed-Na MCMC run:
+#post = np.load("../run/MCMC_WASP17b_hires.npz")
+post = np.load("../retrieval_v03_3HW/MCMC_WASP17b_hires.npz")
 Z = post["Z"]
 Z = Z[burnin:,:]
 Z[:,1] /= rjup
-
-parname = [r"$T\ ({\rm K})$", r"$R_0\ (R_{\rm Jup})$"]
-nsamples, npars = np.shape(Z)
-thinning = 1
-nbins = 35
-nlevels = 20
-posterior = Z[:-1]
-
+nsamples, npars1 = np.shape(Z)
+posterior1 = Z[:-1]
 # Gather 2D histograms:
-hist = []
-xran, yran, lmax = [], [], []
-for   j in np.arange(1, npars): # Rows
-  for i in np.arange(npars-1):  # Columns
+hist1 = []
+xran1, yran1, lmax1 = [], [], []
+for   j in np.arange(1, npars1): # Rows
+  for i in np.arange(npars1-1):  # Columns
     if j > i:
-      h,x,y = np.histogram2d(posterior[0::thinning,i],
-                  posterior[0::thinning,j], bins=nbins, normed=False)
-      hist.append(h.T)
-      xran.append(x)
-      yran.append(y)
-      lmax.append(np.amax(h)+1)
+      h,x,y = np.histogram2d(posterior1[:,i], posterior1[:,j],
+                             bins=nbins, normed=False)
+      hist1.append(h.T)
+      xran1.append(x)
+      yran1.append(y)
+      lmax1.append(np.amax(h)+1)
+
+# Free-Na MCMC run:
+post = np.load("../run/MCMC_WASP17b_hires_abundance.npz")
+Z = post["Z"]
+Z = Z[burnin:,:]
+Z[:,1] /= rjup
+nsamples, npars2 = np.shape(Z)
+posterior2 = Z[:-1]
+# Gather 2D histograms:
+hist2 = []
+xran2, yran2, lmax2 = [], [], []
+for   j in np.arange(1, npars2): # Rows
+  for i in np.arange(npars2-1):  # Columns
+    if j > i:
+      h,x,y = np.histogram2d(posterior2[:,i], posterior2[:,j],
+                             bins=nbins, normed=False)
+      hist2.append(h.T)
+      xran2.append(x)
+      yran2.append(y)
+      lmax2.append(np.amax(h)+1)
 
 
-# Histogram keywords depending whether one wants the HPD or not:
-pdf  = [None]*npars
-xpdf = [None]*npars
-hkw = {}
+# Plotting setup:
+pdf  = [None]*npars2
+xpdf = [None]*npars2
 percentile = 0.683
-hkw = {'histtype':'step', 'lw':2}
-ranges = [None]*npars
+hkw = {'histtype':'step', 'lw':1.5}
+parname = [r"$T\ ({\rm K})$", r"$R_0\ (R_{\rm Jup})$", r"[Na/H]"]
+fs = 10
 
-fs = 11
-fig = plt.figure(20, (8.5, 3))
+
+fig = plt.figure(21, (8.5, 4))
 plt.clf()
-plt.subplots_adjust(0.095, 0.25, 0.98, 0.95, wspace=0.4)
-ax=plt.subplot(131)
+plt.subplots_adjust(0.095, 0.17, 0.98, 0.95, wspace=0.1, hspace=0.1)
 
-h = 1 # Subplot index
-k = 0 # Histogram index
-plt.yticks(size=fs)
+# Fixed-Na MCMC run:
+xran = [(450, 2050),(1.74, 1.88)]
+xticks = [(500, 1000, 1500, 2000), (1.75, 1.8, 1.85)]
+# Pairwise posterior:
+ax=plt.subplot(256)
+k = 0
+a = plt.contourf(hist1[k], cmap=palette, vmin=1, origin='lower',
+            levels=[0]+list(np.linspace(1,lmax1[k], nlevels)),
+            extent=(xran1[k][0], xran1[k][-1], yran1[k][0], yran1[k][-1]))
+plt.xticks(size=fs-1, rotation=90)
+plt.yticks(size=fs-1)
 plt.ylabel(parname[1], size=fs, multialignment='center')
-plt.xticks(size=fs, rotation=90)
 plt.xlabel(parname[0], size=fs)
-# The plot:
-a = plt.contourf(hist[0], cmap=palette, vmin=1, origin='lower',
-            levels=[0]+list(np.linspace(1,lmax[k], nlevels)),
-            extent=(xran[k][0], xran[k][-1], yran[k][0], yran[k][-1]))
+ax.set_xlim(xran[0])
+ax.set_ylim(xran[1])
+ax.set_xticks(xticks[0])
 for c in a.collections:
   c.set_edgecolor("face")
-# The colorbar:
+# Marginal posteriors:
+for i in np.arange(npars1):
+  ax = plt.subplot(2, 5, 1+i*6)
+  a = plt.xticks(size=fs-1, rotation=90)
+  a = plt.yticks(size=fs-1)
+  ax.set_yticks([])
+  if i == 0:
+    ax.set_xticklabels("")
+  else:
+    plt.xlabel(parname[i], size=fs)
+  vals, bins, h = plt.hist(posterior1[:,i], bins=25, normed=False, **hkw)
+  if percentile is not None:
+    PDF, Xpdf, HPDmin = mu.credregion(posterior1[:,i], percentile,
+                                      pdf[i], xpdf[i])
+    vals = np.r_[0, vals, 0]
+    bins = np.r_[bins[0] - (bins[1]-bins[0]), bins]
+    f = si.interp1d(bins+0.5*(bins[1]-bins[0]), vals, kind='nearest')
+    ax.fill_between(Xpdf, 0, f(Xpdf), where=PDF>=HPDmin,
+                 facecolor='0.7', edgecolor='none', interpolate=False)
+  ax.set_xlim(xran[i])
+  ax.set_xticks(xticks[i])
+
+# Free-Na MCMC run:
+xran = [(350,3000), (1.68, 1.95), (-4.05, 3.05)]
+# Pairwise posterior:
+idx = 10, 16, 17
+for k in np.arange(npars2):
+  ax = plt.subplot(3, 6, idx[k])
+  a = plt.contourf(hist2[k], cmap=palette, vmin=1, origin='lower',
+            levels=[0]+list(np.linspace(1,lmax2[k], nlevels)),
+            extent=(xran2[k][0], xran2[k][-1], yran2[k][0], yran2[k][-1]))
+  plt.yticks(size=fs-1)
+  plt.xticks(size=fs-1, rotation=90)
+  if k <2:
+    plt.ylabel(parname[k+1], size=fs, multialignment='center')
+  else:
+    ax.set_yticklabels("")
+  if k > 0:
+    plt.xlabel(parname[k-1], size=fs)
+  else:
+    ax.set_xticklabels("")
+  for c in a.collections:
+    c.set_edgecolor("face")
+# Marginal posteriors:
+for i in np.arange(npars2):
+  ax = plt.subplot(3, 6, 4+i*7)
+  a = plt.xticks(size=fs-1, rotation=90)
+  a = plt.yticks(size=fs-1)
+  ax.set_yticks([])
+  if i == npars2-1:
+    plt.xlabel(parname[i], size=fs)
+  else:
+    ax.set_xticklabels("")
+  vals, bins, h = plt.hist(posterior2[:,i], bins=25, normed=False, **hkw)
+  if percentile is not None:
+    PDF, Xpdf, HPDmin = mu.credregion(posterior2[:,i], percentile,
+                                      pdf[i], xpdf[i])
+    vals = np.r_[0, vals, 0]
+    bins = np.r_[bins[0] - (bins[1]-bins[0]), bins]
+    f = si.interp1d(bins+0.5*(bins[1]-bins[0]), vals, kind='nearest')
+    ax.fill_between(Xpdf, 0, f(Xpdf), where=PDF>=HPDmin,
+                    facecolor='0.7', edgecolor='none', interpolate=False)
+for j in np.arange(npars2):
+  for i in np.arange(j+1):
+    ax = plt.subplot(3, 6, 4+i+6*j)
+    ax.set_xlim(xran[i])
+    if j>i:
+      ax.set_ylim(xran[j])
+# Colorbar:
 bounds = np.linspace(0, 1.0, nlevels)
 norm = mpl.colors.BoundaryNorm(bounds, palette.N)
-ax2 = fig.add_axes([0.34, 0.25, 0.015, 0.7])
+ax2 = fig.add_axes([0.9, 0.525, 0.015, 0.4])
 cb = mpl.colorbar.ColorbarBase(ax2, cmap=palette, norm=norm,
       spacing='proportional', boundaries=bounds, format='%.1f')
 cb.set_label("Posterior density", fontsize=fs)
@@ -85,30 +175,7 @@ cb.set_ticks(np.linspace(0, 1, 5))
 for c in ax2.collections:
   c.set_edgecolor("face")
 
-for i in np.arange(npars):
-  ax = plt.subplot(1, 3, i+2)
-  a = plt.xticks(size=fs-2.0, rotation=90)
-  a = plt.yticks(size=fs-2.0)
-  ax.set_yticklabels([])
-  plt.ylabel(r"$N\ \rm samples$", fontsize=fs)
-  plt.xlabel(parname[i], size=fs)
-  vals, bins, h = plt.hist(posterior[0::thinning, i], bins=25,
-                           range=ranges[i], normed=False, **hkw)
-  # Plot HPD region:
-  if percentile is not None:
-    PDF, Xpdf, HPDmin = mu.credregion(posterior[:,i], percentile,
-                                      pdf[i], xpdf[i])
-    vals = np.r_[0, vals, 0]
-    bins = np.r_[bins[0] - (bins[1]-bins[0]), bins]
-    # interpolate xpdf into the histogram:
-    f = si.interp1d(bins+0.5*(bins[1]-bins[0]), vals, kind='nearest')
-    # Plot the HPD region as shaded areas:
-    if ranges[i] is not None:
-      Xran = np.argwhere((Xpdf>ranges[i][0]) & (Xpdf<ranges[i][1]))
-      Xpdf = Xpdf[np.amin(Xran):np.amax(Xran)]
-      PDF  = PDF [np.amin(Xran):np.amax(Xran)]
-    ax.fill_between(Xpdf, 0, f(Xpdf), where=PDF>=HPDmin,
-                 facecolor='0.7', edgecolor='none', interpolate=False)
-plt.savefig("../plots/posteriors_v02.ps")
+plt.savefig("../plots/WASP-17b_hires_posteriors.pdf")
+
 
 
